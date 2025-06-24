85ad3d4c799af31343fff1243f49c37149d29e27
EugeneMazurok <audiofaun896373@gmail.com>
2025-06-24 13:51:31 +0300

    init

---
+++ surf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  surf
+//
+//  Created by Eugene on 6/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ surf/surfApp.swift
@@ -0,0 +1,17 @@
+//
+//  surfApp.swift
+//  surf
+//
+//  Created by Eugene on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct surfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
